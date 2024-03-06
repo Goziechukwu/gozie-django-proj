706dd25de03b8b69f4ac2f7c5ad84ad5077a3ff3
Goziechukwu <inekwegoziechukwu@gmail.com>
2024-03-06 22:53:48 +0000

    generic post view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
-from django.http import HttpResponse
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
 
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
